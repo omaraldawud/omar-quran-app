bf3443f424b72113a4bcbac179b4caeec9d424dc
omaraldawud <oaldawud@clarku.edu>
2025-11-29 12:06:02 -0600

    Added Surah Audio

--- src/components/SurahViewer.jsx
+++ src/components/SurahViewer.jsx
@@ -11,7 +11,7 @@ export default function SurahViewer({ surah, arabic, translation }) {
   const versesAr = arabic[surah.id];
   const versesTr = translation[surah.id];
 
-  // Generate direct mp3 URL (no API fetch, avoids CORS)
+  // Direct mp3 URL for Surah (no fetch)
   const surahNumber = String(surah.id).padStart(3, "0");
   const audioUrl = `https://download.quranicaudio.com/quran/abdullaah_3awwaad_al-juhaynee//${surahNumber}.mp3`;
 
@@ -34,29 +34,22 @@ export default function SurahViewer({ surah, arabic, translation }) {
           <span className="surah-header">Verses: {surah.total_verses}</span>
           <audio controls src={audioUrl}>
             Your browser does not support the audio element.
-          </audio>{" "}
+          </audio>
         </div>
       </div>
-      <hr class="hr-elegant-islamic" />
 
-      {/* Audio player */}
+      <hr className="hr-elegant-islamic" />
 
-      {/* Verses */}
       {versesAr.map((verse, i) => (
         <div key={i} className="ayah-container mb-4 p-4 rounded my-5">
           <div className="ayah-content py-4 rounded">
             <div className="ayah-header">
-              {/* Ayah number image */}
               <div className="ayah-number-wrapper">
                 <img src={ayahIcon} alt="Ayah" className="ayah-icon" />
                 <span className="ayah-number-text">{verse.verse}</span>
               </div>
-
-              {/* Arabic text */}
               <div className="text-end fs-3 arabic-text">{verse.text}</div>
             </div>
-
-            {/* Translation text */}
             <div className="translation-text mt-3">{versesTr[i].text}</div>
           </div>
         </div>
